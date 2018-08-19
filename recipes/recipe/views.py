5a165cd3b39931516762946008c4d04de607fe42
Lukas Bühler <lukasmc6@gmail.com>
2018-08-19 03:20:56 +0200

    trying to get the context

--- recipes/recipe/views.py
+++ recipes/recipe/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class RecipeView(TemplateView):
+    template_name = 'recipe.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(RecipeView, self).get_context_data(**kwargs)
+        context['id'] = '2'
+        return context
--- recipes/recipes/urls.py
+++ recipes/recipes/urls.py
@@ -14,15 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from .routers import router
+from django.urls import path, re_path, include
+
 from django.views.generic import TemplateView
+from recipe.views import RecipeView
+from .routers import router
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    path(r'recipes/', TemplateView.as_view(template_name='recipes/index.html'), name='recipes'),
-    path(r'recipe/(\d+)/', TemplateView.as_view(template_name='recipe/index.html'), name='recipe_details'),
+    path('recipes/', TemplateView.as_view(template_name='recipes.html')),
+    path('recipe/<int:recipe_id>/', RecipeView.as_view(), name="recipe"),
 ]
